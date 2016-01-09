930fdbfab2e52de781ff05f8e49834630a520a2d
FireeSpigot <jdogdoespvp16@gmail.com>
2016-01-09 17:31:31 -0600

    Add PlayerTab.java :)

---
+++ src/me/jdog/motd/events/PlayerTab.java
@@ -0,0 +1,35 @@
+package me.jdog.motd.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import com.connorlinfoot.titleapi.TitleAPI;
+
+import me.jdog.motd.Custom;
+
+public class PlayerTab implements Listener {
+	
+	private Custom plugin;
+	
+	public PlayerTab(Custom pl) {
+		plugin = pl;
+	}
+	
+	@EventHandler
+	public void PlayerTabEvent(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		String header = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("TabHeader"));
+		String footer = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("TabFooter"));
+		
+		if(plugin.getConfig().getBoolean("ShowTab")==true) {
+			TitleAPI.sendTabTitle(p, header, footer);
+		}
+		else {
+			return;
+		}
+	}
+
+}
