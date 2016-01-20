ee5a69a6ceccf62f3dcf39bf9ae645ead70c39dc
FireeSpigot <jdogdoespvp16@gmail.com>
2016-01-20 11:26:24 -0600

    Update PlayerMOTD.java

--- src/me/jdog/motd/events/PlayerMOTD.java
+++ src/me/jdog/motd/events/PlayerMOTD.java
@@ -6,14 +6,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import me.jdog.motd.MOTD;
 import me.jdog.motd.api.MessageAPI;
-import me.jdog.motd.Custom;
 
 public class PlayerMOTD implements Listener {
 	
-	private Custom plugin;
+	private MOTD plugin;
 	
-	public PlayerMOTD(Custom pl) {
+	public PlayerMOTD(MOTD pl) {
 		plugin = pl;
 	}
 	
@@ -24,7 +24,7 @@ public class PlayerMOTD implements Listener {
 		String welcomeMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("WelcomeMessage"));
 		
 		if (plugin.getConfig().getBoolean("ShowMessage")==true) {
-			MessageAPI.sendMessage(welcomeMessage);
+			MessageAPI.sendMessage(p, welcomeMessage);
 		}
 		else {
 			return;
